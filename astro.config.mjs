0c15d2f2d7dd28f42b43c2d014f020cf7d70624b
delucis <swithinbank@gmail.com>
2022-08-22 09:50:44 +0200

    Update site config logic for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.URL || 'https://localhost:3000/',
+  site:
+    process.env.VERCEL_ENV === 'production'
+      ? 'https://astro.badg.es/'
+      : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/`
+      : 'https://localhost:3000/',
 });
