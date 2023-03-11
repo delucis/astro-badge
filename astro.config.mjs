ba8ca2542694cdf1d68bae7b1733db21dea9cd78
Elian Van Cutsem <hello@elian.codes>
2023-03-11 13:47:16 +0100

    Update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.VERCEL_ENV === 'production' ? 'https://astro.badg.es/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
