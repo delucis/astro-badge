114e6c9a4a0eff7c48ce1003ff308613aee6d273
Chris Swithinbank <swithinbank@gmail.com>
2023-05-17 19:36:20 +0200

    Fix Astro logo

--- src/pages/v1/contributor/[username].svg.ts
+++ src/pages/v1/contributor/[username].svg.ts
@@ -27,16 +27,10 @@ ${icons[type]}
 const Achievement = ({ achievements }: { achievements: { title: string; details: string }[] }, i: number) =>
 `<text x="50" y="${50 + i * 22}">${achievements[0].title} <tspan font-size="12" fill="#bbb">${achievements[0].details}</tspan></text>`
 
-const AstroLogo = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" x="5" y="160" viewBox="0 0 36 36" width="30" height="30">
-  <path fill="#fff" d="M22.25 4h-8.5a1 1 0 0 0-.96.73l-5.54 19.4a.5.5 0 0 0 .62.62l5.05-1.44a2 2 0 0 0 1.38-1.4l3.22-11.66a.5.5 0 0 1 .96 0l3.22 11.67a2 2 0 0 0 1.38 1.39l5.05 1.44a.5.5 0 0 0 .62-.62l-5.54-19.4a1 1 0 0 0-.96-.73Z"/>
-  <path fill="#fff" d="M18 28a7.63 7.63 0 0 1-5-2c-1.4 2.1-.35 4.35.6 5.55.14.17.41.07.47-.15.44-1.8 2.93-1.22 2.93.6 0 2.28.87 3.4 1.72 3.81.34.16.59-.2.49-.56-.31-1.05-.29-2.46 1.29-3.25 3-1.5 3.17-4.83 2.5-6-.67.67-2.6 2-5 2Z"/>
-  <defs>
-    <linearGradient id="gradient" x1="16" x2="16" y1="32" y2="24" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#000"/>
-      <stop offset="1" stop-color="#000" stop-opacity="0"/>
-    </linearGradient>
-  </defs>
-</svg>`
+const AstroLogo = `<svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 64 79" x="6.5" y="162" width="28" height="28">
+  <path d="M20 66c-3.9-3.6-5-11-3.4-16.2 2.8 3.3 6.7 4.4 10.7 5 6.2 1 12.3.6 18-2.2l2-1.2c.6 1.5.7 3.1.5 4.7-.4 3.9-2.4 6.9-5.5 9.1-1.2 1-2.5 1.7-3.8 2.6-4 2.6-5 5.7-3.6 10.2l.2.5c-2-1-3.5-2.2-4.6-4a10.8 10.8 0 0 1-1.8-5.8l-.1-3.2c-.4-2.5-1.6-3.6-3.8-3.7a4.4 4.4 0 0 0-4.7 3.6l-.1.5Z"/>
+  <path d="M.5 51.4s10.6-5.2 21.2-5.2l8-24.7c.3-1.2 1.2-2 2.2-2 1 0 1.9.8 2.2 2l8 24.7a45 45 0 0 1 21.2 5.2l-18-49C44.8.9 43.9 0 42.7 0H21.1c-1.2 0-2 1-2.6 2.4l-18 49Z"/>
+</svg>`;
 
 export async function get({ params }: APIContext): Promise<EndpointOutput> {
   const { username } = params;
