bbe9c2cb4ff84f289647530986001ba8872da7a8
Chris Swithinbank <swithinbank@gmail.com>
2023-07-08 15:30:20 +0200

    Create simple endpoint rendering existing SVG as PNG

---
+++ src/pages/v1/contributor/[username].png.ts
@@ -0,0 +1,15 @@
+import type { APIContext, EndpointOutput } from 'astro';
+import { Resvg } from '@resvg/resvg-js';
+import { get as getSvg } from './[username].svg';
+export { getStaticPaths } from './[username].svg';
+
+export async function get(ctx: APIContext): Promise<EndpointOutput> {
+  const { body: svg } = await getSvg(ctx);
+  const resvg = new Resvg(svg, {
+    fitTo: { mode: 'zoom', value: 2 },
+  });
+  return {
+    body: resvg.render().asPng(),
+    encoding: 'binary',
+  };
+}
