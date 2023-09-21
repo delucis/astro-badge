10fa3b1c4be0562b61ac5c957f3d102f6f959fe5
Chris Swithinbank <swithinbank@gmail.com>
2023-09-21 19:31:37 +0200

    Update individual contributor page

--- src/util/achievementClasses.ts
+++ src/util/achievementClasses.ts
@@ -10,6 +10,15 @@ export const achievementClassGradientFrom = (cls: AchievementClass) =>
   (['from-bronze', 'from-silver', 'from-gold'] as const)[cls];
 export const achievementClassGradientTo = (cls: AchievementClass) =>
   (['to-bronze/50', 'to-silver/50', 'to-gold/50'] as const)[cls];
+export const achievementClassGradient = (cls: AchievementClass, dir = 'bg-gradient-to-br') =>
+  [dir, 'bg-white', achievementClassGradientFrom(cls), achievementClassGradientTo(cls)].join(' ');
+export const achievementClassGradientText = (cls: AchievementClass, dir = 'bg-gradient-to-br') =>
+  [
+    dir,
+    'text-transparent bg-clip-text bg-white',
+    achievementClassGradientFrom(cls),
+    achievementClassGradientTo(cls),
+  ].join(' ');
 
 export const achievementClassSlug = (cls: AchievementClass) =>
   (['bronze', 'silver', 'gold'] as const)[cls];
@@ -17,9 +26,4 @@ export const achievementClassSlug = (cls: AchievementClass) =>
 export const achievementClassLabel = (cls: AchievementClass) =>
   (['Bronze', 'Silver', 'Gold'] as const)[cls];
 
-export const achievementClassEmoji = (cls: AchievementClass) =>
-  (['🥉', '🥈', '🥇'] as const)[cls];
-
-// background: var(--Gold-Gradient, linear-gradient(120deg, #FFCA58 0%, rgba(255, 202, 88, 0.50) 100%), #FFF);
-// background: var(--Silver-Gradient, linear-gradient(120deg, #BFC1C9 0%, #D0D1D7 0.01%, rgba(208, 209, 215, 0.50) 100%), #FFF);
-// background: var(--Bronze-Gradient, linear-gradient(120deg, #FF9E58 0%, rgba(255, 158, 88, 0.50) 100%), #FFF);
+export const achievementClassEmoji = (cls: AchievementClass) => (['🥉', '🥈', '🥇'] as const)[cls];
