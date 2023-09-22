62d21653fd8d0b9de64e6f6d7d01524e8e7c3cd6
Chris Swithinbank <swithinbank@gmail.com>
2023-09-22 12:35:06 +0200

    Refactor uses of gradient utility

--- src/util/achievementClasses.ts
+++ src/util/achievementClasses.ts
@@ -1,11 +1,5 @@
 import type { AchievementClass } from './achievementsHelpers';
 
-export const achievementClassBg = (cls: AchievementClass) =>
-  (['bg-bronze', 'bg-silver', 'bg-gold'] as const)[cls];
-
-export const achievementClassText = (cls: AchievementClass) =>
-  (['text-bronze', 'text-silver', 'text-gold'] as const)[cls];
-
 export const achievementClassGradientFrom = (cls: AchievementClass) =>
   (['from-bronze', 'from-silver', 'from-gold'] as const)[cls];
 export const achievementClassGradientTo = (cls: AchievementClass) =>
