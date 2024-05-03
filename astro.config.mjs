c9d1fac41072b5d705252e3e97db1a28b60e8938
Chris Swithinbank <swithinbank@gmail.com>
2024-05-04 00:33:00 +0200

    Discard changes to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,11 @@
-import db from '@astrojs/db';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import webVitals from '@astrojs/web-vitals';
 import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.VERCEL_ENV === 'production' ? 'https://astro.badg.es/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'http://localhost:4321/',
-  output: 'hybrid',
-  adapter: vercel(),
-  trailingSlash: 'always',
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    db(),
-    webVitals(),
-  ],
+  integrations: [tailwind({ applyBaseStyles: false })],
   vite: {
     ssr: { external: ['@resvg/resvg-js'] },
     optimizeDeps: { exclude: ['@resvg/resvg-js'] },
