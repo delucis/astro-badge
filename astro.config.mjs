87e188e924d9c93f123b3d61714a6815187e41b5
Chris Swithinbank <swithinbank@gmail.com>
2025-09-01 11:55:16 +0200

    Update to Astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.VERCEL_ENV === 'production' ? 'https://astro.badg.es/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'http://localhost:4321/',
-  output: 'hybrid',
   adapter: vercel(),
   integrations: [
     tailwind({ applyBaseStyles: false }),
